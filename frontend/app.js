a073bd4b79430bf8f4ad27d9d2612b5ccf0bf2c7
Noah Frank <noahdwfrank@gmail.com>
2026-03-10 20:46:27 -0600

    fixed api_base

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:5000";
+const API_BASE = "https://nutrition-backend.salmonsky-a7f44c9d.canadacentral.azurecontainerapps.io";
 
 const dietInput = document.getElementById("dietInput");
 const dietSelect = document.getElementById("dietSelect");
